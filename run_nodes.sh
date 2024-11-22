9e142195bce40e7f50fcdb1eaac4e24768b5c4cc
PrastianHD <48909279+PrastianHD@users.noreply.github.com>
2024-11-22 22:47:12 +0700

    Update run_nodes.sh

--- run_nodes.sh
+++ run_nodes.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 
 # Nama folder utama
-main_folder="Glacier"
-
-# Pindah ke folder utama
-cd $main_folder
+main_folder="$(pwd)" # Mendapatkan path absolut dari folder saat ini
 
 # Buat file JSON untuk PM2
 pm2_config="pm2-glacier.json"
 echo "[" > $pm2_config
 
 # Loop untuk setiap node
-for i in $(ls -d node*/ | sed 's#/##'); do
+for node_dir in "$main_folder"/node*/; do
+  node_name=$(basename "$node_dir")
+  config_file="$node_dir/config.yaml"
+  binary_file="$node_dir/verifier_linux_amd64"
+
+  if [ ! -f "$config_file" ]; then
+    echo "Config file not found in $config_file. Skipping $node_name."
+    continue
+  fi
+
   echo "  {
-    \"name\": \"glacier-$i\",
-    \"script\": \"$i/verifier_linux_amd64\",
-    \"args\": \"--config $i/config.yaml\"
+    \"name\": \"glacier-$node_name\",
+    \"script\": \"$binary_file\",
+    \"args\": \"--config $config_file\"
   }," >> $pm2_config
 done
 
@@ -29,8 +35,4 @@ pm2 start $pm2_config
 # Simpan konfigurasi PM2
 pm2 save
 
-# Kembali ke direktori awal
-cd ..
-
-# Informasi selesai
-echo "Semua node dijalankan dengan PM2."
+echo "Semua node dijalankan dengan PM2 menggunakan konfigurasi di $main_folder."
