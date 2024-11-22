a6c9667022245636a820da9ff47f3d53b32f90c0
PrastianHD <48909279+PrastianHD@users.noreply.github.com>
2024-11-23 00:31:30 +0700

    Update run_nodes.sh

--- run_nodes.sh
+++ run_nodes.sh
@@ -18,10 +18,9 @@ echo "[" > $pm2_config
 
 # Loop untuk setiap folder node
 for node_dir in "$main_folder"/node*/; do
-  # Pastikan tidak ada garis miring ganda
   node_dir="${node_dir%/}"
   node_name=$(basename "$node_dir")
-  config_file="$node_dir/config.yaml"
+  config_file="${node_dir%/}/config.yaml"
 
   # Verifikasi bahwa file config.yaml ada
   if [ ! -f "$config_file" ]; then
